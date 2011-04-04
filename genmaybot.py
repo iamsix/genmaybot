c4aa13455e4103c0eab844a7c56d8ff6f70b7f77
fitty six <github@sixis.ca>
2011-04-04 11:34:35 -0600

    change formatting of !stock output

--- genmaybot.py
+++ genmaybot.py
@@ -537,20 +537,21 @@ class TestBot(SingleServerIRCBot):
     
       opener = urllib2.build_opener()
       opener.addheaders = [('User-Agent',"Opera/9.10 (YourMom 8.0)")]
-      pagetmp = opener.open("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=l1c1va2j1" % stock)
+      pagetmp = opener.open("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nl1c1va2j1" % stock)
       quote = pagetmp.read(1024)
       opener.close()
     
-      price,change,volume,avg_volume,mkt_cap = quote.split(",")
+      name,price,change,volume,avg_volume,mkt_cap = quote.split(",")
       if price != "0.00": #assume no price = no result
+         name = name.replace('"','')
       
          if change != "N/A":
-             change = change + '({0:.2%})'.format((float(change)/(float(price) - float(change))))
+             change = change + ' ({0:.2%})'.format((float(change)/(float(price) - float(change))))
          if volume != "N/A":
              volume = '{0:n}'.format(int(volume))
              avg_volume = '{0:n}'.format(int(avg_volume))
          
-         return "[%s] Price: %s Change: %s Cap: %s Volume (Avg): %s (%s)" % (stock,price,change,mkt_cap.strip(),volume,avg_volume)
+         return "[%s] %s    %s %s | Cap: %s | Volume (Avg): %s (%s)" % (stock,name.strip(),price,change,mkt_cap.strip(),volume,avg_volume)
 
     def get_imdb(self, searchterm, urlposted=False):
         title = ""

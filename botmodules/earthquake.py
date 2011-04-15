7c1c6ef16b995f0e403e9b13c59cc4ecafe788f0
fitty six <github@sixis.ca>
2011-04-15 13:28:04 -0600

    OO-ify quake code

--- botmodules/earthquake.py
+++ botmodules/earthquake.py
@@ -4,13 +4,7 @@ import urllib2, xml.dom.minidom, datetime
 def get_quake(nothing):
     #returns the latest earthquake on USGS
       try:       
-        request = urllib2.urlopen("http://earthquake.usgs.gov/earthquakes/catalogs/1day-M2.5.xml")
-        dom = xml.dom.minidom.parse(request)
-
-        latest_quakenode = dom.getElementsByTagName('entry')[0]
-        qtitle = latest_quakenode.getElementsByTagName('title')[0].childNodes[0].data
-        request.close()
-        
+        qtitle,updated = get_quake_data()        
         return "Latest Earthquake: " + qtitle  
       except:
         pass
@@ -18,15 +12,9 @@ get_quake.command = "!q"
 
 
 def quake_alert():
-    #returns a new quake only if it hasn't returned it before - for use in alerts
-      try:       
-        request = urllib2.urlopen("http://earthquake.usgs.gov/earthquakes/catalogs/1day-M2.5.xml")
-        dom = xml.dom.minidom.parse(request)
-        latest_quakenode = dom.getElementsByTagName('entry')[0]
-        updated = latest_quakenode.getElementsByTagName('updated')[0].childNodes[0].data
-        qtitle = latest_quakenode.getElementsByTagName('title')[0].childNodes[0].data
-        updated = datetime.datetime.strptime(updated, "%Y-%m-%dT%H:%M:%SZ")
-        request.close()
+    #returns a new get_quake_data only if it hasn't returned it before - for use in alerts
+      try:
+        qtitle,updated = get_quake_data()
         if not quake_alert.lastquakecheck:
             quake_alert.lastquakecheck = updated
         if updated > quake_alert.lastquakecheck :
@@ -35,6 +23,16 @@ def quake_alert():
       except Exception as inst: 
           print "quakealert: " + str(inst)
           pass
-
 quake_alert.lastquakecheck = ""
 quake_alert.alert = True
+
+
+def get_quake_data():
+    request = urllib2.urlopen("http://earthquake.usgs.gov/earthquakes/catalogs/1day-M2.5.xml")
+    dom = xml.dom.minidom.parse(request)
+    latest_quakenode = dom.getElementsByTagName('entry')[0]
+    updated = latest_quakenode.getElementsByTagName('updated')[0].childNodes[0].data
+    qtitle = latest_quakenode.getElementsByTagName('title')[0].childNodes[0].data
+    updated = datetime.datetime.strptime(updated, "%Y-%m-%dT%H:%M:%SZ")
+    request.close()
+    return qtitle, updated

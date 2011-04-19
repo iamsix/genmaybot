ea24f53d54e385a53795e8a02de82fc929a36bdb
cactauz <deansauce@gmail.com>
2011-04-19 16:12:28 -0600

    added random error messages thing, probably will integrate to send errors to the channel for certain things (e.x. bad wolframs)

---
+++ botmodules/error_generator.py
@@ -0,0 +1,43 @@
+import random
+
+def error_generator(nothing, nick):
+    firstword = firstwords[random.randint(0, len(firstwords) - 1)]
+    secondword = secondwords[random.randint(0, len(secondwords) - 1)]
+    thirdword = secondwords[random.randint(0, len(thirdwords) - 1)]
+    fourthword = fourthwords[random.randint(0, 4)]
+    return "%s %s %s %s" % (firstword, secondword, thirdword, fourthword)
+    
+error_generator.command = "!error"
+
+firstwords = ["Temporary", "Intermittant", "Partial", "Redundant", "Total", 
+                "Multiplexed", "Inherent", "Duplicated", "Dual-Homed", "Synchronous", 
+                "Bidirectional", "Serial", "Asynchronous", "Multiple", "Replicated", 
+                "Non-Replicated", "Unregistered", "Non-Specific", "Generic", "Migrated", 
+                "Localised", "Resignalled", "Dereferenced", "Nullified", "Aborted", 
+                "Serious", "Minor", "Major", "Extraneous", "Illegal", "Insufficient", 
+                "Viral", "Unsupported", "Outmoded", "Legacy", "Permanent", "Invalid", 
+                "Deprecated", "Virtual", "Unreportable", "Undetermined", "Undiagnosable", 
+                "Unfiltered", "Static", "Dynamic", "Delayed", "Immediate", "Nonfatal", 
+                "Fatal", "Non-Valid", "Unvalidated", "Non-Static", "Unreplicatable", 
+                "Non-Serious"]
+
+secondwords = ["Array", "Systems", "Hardware", "Software", "Firmware", 
+                "Backplane", "Logic-Subsystem", "Integrity", "Subsystem", "Memory", 
+                "Comms", "Integrity", "Checksum", "Protocol", "Parity", "Bus", "Timing", 
+                "Synchronisation", "Topology", "Transmission", "Reception", "Stack", 
+                "Framing", "Code", "Programming", "Peripheral", "Environmental", 
+                "Loading", "Operation", "Parameter", "Syntax", "Initialisation", 
+                "Execution", "Resource", "Encryption", "Decryption", "File", 
+                "Precondition", "Authentication", "Paging", "Swapfile", "Service", 
+                "Gateway", "Request", "Proxy", "Media", "Registry", "Configuration", 
+                "Metadata", "Streaming", "Retrieval", "Installation", "Library", "Handler"]
+
+thirdwords = ["Interruption", "Destabilisation", "Destruction", 
+                "Desynchronisation", "Failure", "Dereferencing", "Overflow", "Underflow", 
+                "NMI", "Interrupt", "Corruption", "Anomoly", "Seizure", "Override", 
+                "Reclock", "Rejection", "Invalidation", "Halt", "Exhaustion", "Infection", 
+                "Incompatibility", "Timeout", "Expiry", "Unavailability", "Bug", 
+                "Condition", "Crash", "Dump", "Crashdump", "Stackdump", "Problem", 
+                "Lockout"]
+
+fourthwords = ["Error", "Problem", "Warning", "Signal", "Flag"]

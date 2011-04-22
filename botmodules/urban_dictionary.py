505b71cf003a4e622491839ef0383d795840d7d4
KpaBap <kpabap@gmail.com>
2011-04-22 15:24:33 -0700

    fixing more bugs

--- botmodules/urban_dictionary.py
+++ botmodules/urban_dictionary.py
@@ -12,50 +12,50 @@ def get_urbandictionary(searchterm, nick):
     if searchterm== "random":
       url = "http://www.urbandictionary.com/random.php"
     
-    #try:
-    opener = urllib2.build_opener()
-    opener.addheaders = [('User-Agent',"Opera/9.10 (YourMom 8.0)")]
-    pagetmp = opener.open(url)
-    page = pagetmp.read()
-    opener.close()
-
-    page = BeautifulSoup(page)
-    first_definition= ""
-    
-    ## depending on the search results the first word may be contained directly under the <td class='word'> tag
-    ## or it may be the text contents of a <a href> tag
-    ## we first try to get it from inside a <td><a href>[word]</a></td> type structure
-    ## if that fails, get the word under the initial <td> tag
-
     try:
-      first_word = page.findAll('td',attrs={"class" : "word"})[0].contents[1].string
-    except:
-      first_word = page.findAll('td',attrs={"class" : "word"})[0].contents[0].string     
-    
-    first_word = first_word.replace("\n","")
-    first_word = first_word.encode("utf-8", 'ignore')
+      opener = urllib2.build_opener()
+      opener.addheaders = [('User-Agent',"Opera/9.10 (YourMom 8.0)")]
+      pagetmp = opener.open(url)
+      page = pagetmp.read()
+      opener.close()
 
-    for content in page.findAll('div',attrs={"class" : "definition"})[0].contents:
-      if content.string != None:
-        first_definition += content.string
-    
-    
-    first_definition = first_definition.encode("utf-8", 'ignore')
-    first_definition = tools.decode_htmlentities(first_definition.decode("utf-8", 'ignore')).encode("utf-8", 'ignore')
-    first_word = tools.decode_htmlentities(first_word.decode("utf-8", 'ignore')).encode("utf-8", 'ignore')
-    
-    first_definition = first_definition.replace("\n", " ")
-    first_definition = first_definition.replace("\r", " ")
-    first_definition = first_definition[0:392]
-    
-    first_definition = (first_word + ": " + first_definition.decode('utf-8') + " [ %s ]" % tools.shorten_url(url)).encode('utf-8', 'ignore')
-    #print first_definition
-
-    return first_definition
+      page = BeautifulSoup(page)
+      first_definition= ""
       
-    #xcept:
-    #  print "!ud %s went wrong" % searchterm
-    #  return
+      ## depending on the search results the first word may be contained directly under the <td class='word'> tag
+      ## or it may be the text contents of a <a href> tag
+      ## we first try to get it from inside a <td><a href>[word]</a></td> type structure
+      ## if that fails, get the word under the initial <td> tag
+
+      try:
+        first_word = page.findAll('td',attrs={"class" : "word"})[0].contents[1].string
+      except:
+        first_word = page.findAll('td',attrs={"class" : "word"})[0].contents[0].string     
+      
+      first_word = first_word.replace("\n","")
+      first_word = first_word.encode("utf-8", 'ignore')
+
+      for content in page.findAll('div',attrs={"class" : "definition"})[0].contents:
+        if content.string != None:
+          first_definition += content.string
+      
+      
+      first_definition = first_definition.encode("utf-8", 'ignore')
+      first_definition = tools.decode_htmlentities(first_definition.decode("utf-8", 'ignore')).encode("utf-8", 'ignore')
+      first_word = tools.decode_htmlentities(first_word.decode("utf-8", 'ignore')).encode("utf-8", 'ignore')
+      
+      first_definition = first_definition.replace("\n", " ")
+      first_definition = first_definition.replace("\r", " ")
+      first_definition = first_definition[0:392]
+      
+      first_definition = (first_word + ": " + first_definition.decode('utf-8') + " [ %s ]" % tools.shorten_url(url)).encode('utf-8', 'ignore')
+      #print first_definition
+
+      return first_definition
+      
+    except:
+      print "!ud %s went wrong" % searchterm
+      return
 
 get_urbandictionary.command = "!ud" 
 

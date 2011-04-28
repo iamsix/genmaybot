20f9e3495644756f718402a0fba0ad4066fb61af
KpaBap <kpabap@gmail.com>
2011-04-28 15:20:45 -0700

    fixing cactus' bad programming in test kick module

--- botmodules/kick.py
+++ botmodules/kick.py
@@ -1,3 +1,4 @@
 def do_kick(self, e):
-  self.irccontext.kick(#auzland, e.input, "thank you for participating in this test")
+  self.irccontext.kick("#auzland", e.input, "thank you for participating in this test")
+  
 do_kick.command = "!kick"

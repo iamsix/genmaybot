1ade0dcf9eaf077a0f330c52d2cca269469dbbe1
KpaBap <kpabap@gmail.com>
2011-04-30 22:43:23 -0700

    Made weather alerts less spammy and fixed some bugs

--- botmodules/weather_alerts.py
+++ botmodules/weather_alerts.py
@@ -1,4 +1,4 @@
-import urllib2, xml.dom.minidom, datetime, botmodules.tools as tools
+import urllib2, xml.dom.minidom, datetime, botmodules.tools as tools, re
 from dateutil.parser import parse as dateparser
 
 
@@ -32,12 +32,15 @@ def find_latest_weather_alert():
     cur_entry = 0
 
     for entry in weather_entries:
+      urgency = entry.getElementsByTagName('cap:urgency')[0].childNodes[0].data
+      severity = entry.getElementsByTagName('cap:severity')[0].childNodes[0].data
       updated = entry.getElementsByTagName('updated')[0].childNodes[0].data  
       updated = dateparser(updated)
       updated = (updated - updated.utcoffset()).replace(tzinfo=None)
       cur_entry+=1
       
-      if latest_update < updated:
+      ## only get the latest immediate and severe alerts, too much spam otherwise
+      if latest_update < updated and urgency=="Immediate" and severity=="Severe":
         latest_update = updated
         latest_entry = cur_entry
         alert_url = entry.getElementsByTagName('id')[0].childNodes[0].data 
@@ -53,23 +56,35 @@ def get_weather_alert_data(alert_url):
     request = urllib2.urlopen(alert_url)
     dom = xml.dom.minidom.parse(request)
     msgType = dom.getElementsByTagName('msgType')[0].childNodes[0].data
-    note = dom.getElementsByTagName('note')[0].childNodes[0].data
+    note = dom.getElementsByTagName('description')[0].childNodes[0].data
+    note = note.replace("\n"," ")
+    pattern = re.compile("\s+")
+    note = pattern.sub(" ", note)
+    
     event = dom.getElementsByTagName('event')[0].childNodes[0].data
     urgency = dom.getElementsByTagName('urgency')[0].childNodes[0].data
     severity = dom.getElementsByTagName('severity')[0].childNodes[0].data
     certainty = dom.getElementsByTagName('certainty')[0].childNodes[0].data
+    senderName = dom.getElementsByTagName('senderName')[0].childNodes[0].data
+    
+   
     updated = dom.getElementsByTagName('sent')[0].childNodes[0].data
-
     updated = dateparser(updated)
     updated = (updated - updated.utcoffset()).replace(tzinfo=None)
     ago = (datetime.datetime.utcnow() - updated).seconds/60
 
 
     short_url = tools.shorten_url(alert_url)
-    alert_text = "[Weather] %s: %s Urgency: %s Severity: %s Certainty: %s | %s (%s minutes ago)" % (msgType, event, urgency, severity, certainty, note[0:150], ago)
+    ## old text, too verbose
+    ##alert_text = "[%s] %s: %s Urgency: %s Severity: %s Certainty: %s | %s (%s minutes ago)" % (senderName, msgType, event, urgency, severity, certainty, note[0:170], ago)
+    ## new text is self limiting to the IRC limit of 428 characters
     
+    alert_text_start = "[%s] %s: %s" % (senderName, msgType, event)
+    alert_text_end = "(%s minutes ago) [ %s ]" % (ago, short_url)
     
-    return "%s [ %s ]" % (alert_text, short_url)
+    alert_text = "%s | %s %s" % (alert_text_start, note[:425-(len(alert_text_start+alert_text_end))], alert_text_end)
+    return alert_text
+    
   except Exception as inst:
     print "get_weather_alert_data: " + str(inst)
     pass    

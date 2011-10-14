628026603d81af69f9f63ad17996e474975abad0
fitty six <github@sixis.ca>
2011-10-14 13:53:07 -0600

    slightly modify query to allow for zipcode sunset/sunrise

--- botmodules/sun.py
+++ botmodules/sun.py
@@ -20,7 +20,7 @@ def google_sun(term, sun, nick):
     if term == "" and user:
        term = user.get_location(nick)
     term = urllib.quote(term)
-    url = "http://www.google.com/search?hl=en&client=opera&hs=6At&rls=en&q=%s+in+%s&aq=f&aqi=g1&aql=&oq=&gs_rfai=" % (sun, term)
+    url = "http://www.google.com/search?hl=en&client=opera&hs=6At&rls=en&q=%s+%s&aq=f&aqi=g1&aql=&oq=&gs_rfai=" % (sun, term)
     request = urllib2.Request(url, None, {})
     request.add_header('User-Agent', "Opera/9.80 (Windows NT 6.0; U; en) Presto/2.2.15 Version/10.10")
     request.add_header('Range', "bytes=0-40960")

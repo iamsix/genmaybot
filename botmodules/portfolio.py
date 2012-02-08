1e685d29eaaa419cfe3cb80f218ff69b6efec3b6
Iavor Todorov <kpabap@gmail.com>
2012-02-08 11:13:09 -0800

    Signed-off-by: Iavor Todorov <kpabap@gmail.com>

---
+++ botmodules/portfolio.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def add_stock(self, e):
+	
+	#conn = sqlite3.connect('portfolios.sqlite')
+	#c = conn.cursor()
+	#result = c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='portfolios';").fetchone()
+	#if not result:
+	#	c.execute('''create table portfolios(user text, stock text UNIQUE ON CONFLICT REPLACE, numshares integer, pricepaid real)''')
+	
+	pass	
+	#c.execute("INSERT INTO portfolios 
+
+def portfolio(self, e):
+	return e
+
+portfolio.command = "!portfolio"

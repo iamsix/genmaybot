b75a8c477aaab1d6b35d78c5c2a396fb244ce4af
KpaBap <kpabap@gmail.com>
2012-02-09 18:48:50 -0800

    Signed-off-by: KpaBap <kpabap@gmail.com>

--- botmodules/portfolio.py
+++ botmodules/portfolio.py
@@ -103,7 +103,7 @@ def list_stock(nick):
 
 		
 		for stock in result:
-			return_line += "%s%s%s%s%s\n" % (stock[0],stock[1].center(10),str(stock[2]).center(15),str(stock[3]).center(12),str(stock_prices[id_counter]).center(13))
+			#return_line += "%s%s%s%s%s\n" % (stock[0],stock[1].center(10),str(stock[2]).center(15),str(stock[3]).center(12),str(stock_prices[id_counter]).center(13))
 			id_counter+=1
 		return return_line
 	else: 

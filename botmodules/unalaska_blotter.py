f79a76fc34e9425ca5daf2eaead02f019b12878c
KpaBap <kpabap@gmail.com>
2012-04-07 10:40:57 -0700

    Signed-off-by: KpaBap <kpabap@gmail.com>

--- botmodules/unalaska_blotter.py
+++ botmodules/unalaska_blotter.py
@@ -18,13 +18,13 @@ def get_unalaska_blotter(self, e):
 		blotdate = firstBlot.findAll('span',attrs={'class' : 'date'})[0].string
 		details = firstBlot.findAll('span',attrs={'class' : 'details'})[0].string
 	except:
-		print "\nSomething went wrong with processing the blotter page in unalaska_blotter.py\n"
 		pass
-
-
-
+	
+	#Convert date to a bit shorter format
+	blotdate = blotdate.replace(".","")
+	blotdate = time.strftime("%H:%M %a %m/%d/%y",time.strptime(s, "%A %d %B %Y, %I:%M %p"))
+	
 	e.output = "%s [%s] %s" % (headline,blotdate,details)
-	return e
 
 get_unalaska_blotter.command="!blot"
-get_unalaska_blotter.helptext = "Usage: !blot\nRetrieve the latest witty police blotter entry from the city of Unalaska, Alaska"
+get_unalaska_blotter.helptext = "Usage: !blot\n Retrieve the latest witty police blotter entry from the city of Unalaska, Alaska"

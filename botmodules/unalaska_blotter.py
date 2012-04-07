fb5682715e7a7f7014f223e96846710b43cca161
KpaBap <kpabap@gmail.com>
2012-04-07 10:13:28 -0700

    Signed-off-by: KpaBap <kpabap@gmail.com>

---
+++ botmodules/unalaska_blotter.py
@@ -0,0 +1,26 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+
+def get_unalaska_blotter(self, e):
+	url = "http://kucb.org/community/blotter/"
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-Agent',"Opera/9.10 (YourMom 8.0)")]
+	pagetmp = opener.open(url)
+	page = pagetmp.read()
+	opener.close()
+
+	page = BeautifulSoup(page)
+	try:
+		blots = page.findAll('div',attrs={'id' : 'blots'})[0]
+		firstBlot = blots.findAll('div',attrs={'class' : 'blot'})[0]
+
+		headline = firstBlot.findAll('span',attrs={'class' : 'headline small'})[0].string
+		blotdate = firstBlot.findAll('span',attrs={'class' : 'date'})[0].string
+		details = firstBlot.findAll('span',attrs={'class' : 'details'})[0].string
+	except:
+		pass
+	
+	e.output = "%s [%s] %s" % (headline,blotdate,details)
+
+get_unalaska_blotter.command="!blot"
+get_unalaska_blotter.helptext = "Usage: !blot\n Retrieve the latest witty police blotter entry from the city of Unalaska, Alaska"

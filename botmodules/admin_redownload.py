af6f6039008d054aa80986048dbe2da63aa87bdc
KpaBap <kpabap@gmail.com>
2012-07-25 07:41:10 -0700

    Update botmodules/admin_redownload.py

--- botmodules/admin_redownload.py
+++ botmodules/admin_redownload.py
@@ -2,7 +2,7 @@ import tempfile, os, zipfile, urllib.request, urllib.parse, urllib.error, shutil
 
 def redownload_modules(x,y,self,z):
 
-  url = 'https://github.com/KpaBap/genmaybot/zipball/webUI'
+  url = 'https://github.com/KpaBap/genmaybot/zipball/master'
   tmpdir = tempfile.mkdtemp()
   tmpfile = tmpdir+"/bot.zip"
 

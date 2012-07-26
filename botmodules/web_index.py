723f50935dd23f2504ce259af03511d92b3138c0
fittysix <github@sixis.ca>
2012-07-26 08:32:28 -0600

    Disable cherrypy autoreload
    currently cherrypy is crashing when we try to redownload the bot or
    change modules due to the autoreload, so disabling for now

--- botmodules/web_index.py
+++ botmodules/web_index.py
@@ -3,14 +3,14 @@ import threading
 from botmodules.web_auth import AuthController, require, member_of, name_is
 
 class RestrictedArea:
-    
+
     # all methods in this controller (and subcontrollers) is
     # open only to members of the admin group
-    
+
     _cp_config = {
         'auth.require': [member_of('admin')]
     }
-    
+
     @cherrypy.expose
     def index(self):
         return """This is the admin only area."""
@@ -21,25 +21,25 @@ class emptyObject:
         return
 
 class Root:
-    
-   
+
+
     _cp_config = {
         'tools.sessions.on': True,
         'tools.auth.on': True
     }
-    
+
     def __init__(self,bot): #make a reference to the main bot object
         self.bot = bot
-    
+
     auth = AuthController()
-    
+
     restricted = RestrictedArea()
-    
+
     @cherrypy.expose
     @require()
     def index(self):
         return """
-                
+
         <center>
         <a href="/commands" target="output">Commands</a>&nbsp;&nbsp;&nbsp;
         <a href="/botconfig" target="output">Bot Config</a>&nbsp;&nbsp;&nbsp;
@@ -47,47 +47,47 @@ class Root:
         <a href="/error_log" target="output">Error log</a>&nbsp;&nbsp;&nbsp;
        <br><br>
        <iframe name="output" width="1024px" height="768px"></iframe>
-        
+
         """
-    
-        
+
+
     @cherrypy.expose
     @require()
     def event_log(self):
         return "<pre>"+open(self.bot.botconfig['misc']['event_log'],"r").read()+"</pre>"
-    
+
     @cherrypy.expose
     @require()
     def error_log(self):
         return "<pre>"+open(self.bot.botconfig['misc']['error_log'],"r").read()+"</pre>"
 
-    
+
     @cherrypy.expose
     @require()
     def commands(self):
         commandlist = ""
-    
+
         for command in self.bot.bangcommands:
             try: helptext=self.bot.bangcommands[command].helptext.replace("\n","<br>")
             except: helptext=""
-            
+
             commandlist+= "<tr>%s - %s<br/><br/></tr>" % (command,helptext)
-        
+
         return "<table><td>%s</td></table>" % commandlist
-    
+
     @cherrypy.expose
     @require()
     def botconfig(self):
         output=""
-    
+
         for section in self.bot.botconfig:
                 output+="[%s]<br>" % section
                 for item in self.bot.botconfig[section]:
                     output+="%s=%s<br>" % (item,self.bot.botconfig[section][item])
-        
+
         return output
-        
-    
+
+
 
     # This is only available if the user name is joe _and_ he's in group admin
     @cherrypy.expose
@@ -96,9 +96,10 @@ class Root:
     def only_for_joe_admin(self):
         return """Hello Joe Admin - this page is available to you only"""
 
-        
+
 def __init__(bot):
     ##Disable cherrypy logging to stdout, bind to all IPs, start in a separate thread
+    cherrypy.config.update({'engine.autoreload_on': False})
     cherrypy.log.screen=False
     cherrypy.server.socket_host = "0.0.0.0"
     thread = threading.Thread(target=cherrypy.quickstart, args=(Root(bot),))

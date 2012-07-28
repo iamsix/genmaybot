61bf359943da62cf73097ec645bf428b1e8c59ef
cactauz <deansauce@gmail.com>
2012-07-28 13:53:58 -0600

    Update botmodules/strava.py

--- botmodules/strava.py
+++ botmodules/strava.py
@@ -116,7 +116,7 @@ def strava_line_parser(self, e):
                 return e
     else:
         return
-strava_line_parser.lineparser = True
+strava_line_parser.lineparser = False
 
 
 def strava_set_athlete(self, e):

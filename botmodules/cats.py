1a80246fc9118161ebddacfa32f4f49317cf2e4b
Dan Vu Quoc <dan@vuquoc.com>
2012-08-23 08:11:29 -0700

    Tabs lol, silly Dan.

--- botmodules/cats.py
+++ botmodules/cats.py
@@ -62,8 +62,8 @@ def get_redditpics(url):
     catlist = []
     for cat in cats['data']['children']:
         if 'jpg' in cat['data']['url'] or 'imgur.com' in cat['data']['url']:
-			pic_title = cat['data']['title']
-			pic_title.replace('\n', '')
+            pic_title = cat['data']['title']
+            pic_title.replace('\n', '')
             catlist.append(cat['data']['url'] + " - " + pic_title)
 
     cats = catlist.pop(random.randint(0, len(catlist) - 1)) + " :: " + catlist.pop(random.randint(0, len(catlist) - 1))

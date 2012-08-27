f4acecc3b26ac1ed0c32c949ecff9aa3988bedae
KpaBap <kpabap@gmail.com>
2012-08-27 13:18:11 -0700

    Adding NPR Science feed

--- botmodules/news.py
+++ botmodules/news.py
@@ -82,3 +82,40 @@ def get_breaking_data():
     ago = round((datetime.datetime.utcnow() - updated).seconds/60)
     request.close()
     return description, updated, ago
+
+def npr_science(self, e):
+    ## Grab the latest entry from the NPR Health and Science RSS feed
+    
+    url = "http://www.npr.org/rss/rss.php?id=1007"
+    dom = xml.dom.minidom.parse(urllib.request.urlopen(url))
+    newest_news = dom.getElementsByTagName('item')[0]
+    title = newest_news.getElementsByTagName('title')[0].childNodes[0].data
+    description = BeautifulSoup(newest_news.getElementsByTagName('description')[0].childNodes[0].data)
+
+    links = description.findAll('a')
+    for link in links:
+        link.extract()
+    links = description.findAll(color='#6f6f6f')
+    for link in links:
+        link.extract()
+
+    title = title.strip()
+
+    description = str(description)
+    description = description.replace("\n", "")
+
+    description = self.tools['remove_html_tags'](description)
+#    description = tools.decode_htmlentities(description)
+    description = description[0:len(description) - 9]
+    description = description.strip()
+    if description.rfind(".") != -1:
+        description = description[0:description.rfind(".") + 1]
+
+    link = self.tools['shorten_url'](newest_news.getElementsByTagName('link')[0].childNodes[0].data)
+    
+    e.output = "%s - %s [ %s ]" % (title, description, link)
+
+    return e
+
+npr_science.command="!npr-sci"
+npr_science.helptext="Usage: !npr-sci\nShows the latest entry from the NPR Health and Science RSS feed"

3c1a800444b7e49c19bb0fb14b6bc0ad52885137
fittysix <github@sixis.ca>
2012-08-27 10:07:06 -0600

    Change to World Weather Online
    The old google 'ig' weather API is dead and gone, we're now using world
    weather API: http://www.worldweatheronline.com/free-weather-feed.aspx

--- botmodules/weather.py
+++ botmodules/weather.py
@@ -1,74 +1,50 @@
 import urllib.request, urllib.error, urllib.parse, urllib, xml.dom.minidom
-try: import botmodules.userlocation as user
-except: pass
+import json
+try:
+    import botmodules.userlocation as user
+except:
+    pass
+
 
 def get_weather(self, e):
-    #google weather of place specified in 'zip'
-   zip = e.input
-   
-   if zip == "" and user:
-       zip = user.get_location(e.nick)
+    # WWO weather of place specified in 'zip'
+    # http://www.worldweatheronline.com/free-weather-feed.aspx
 
-   url = "http://www.google.com/ig/api?weather=" + urllib.parse.quote(zip)
-   
-   try:
-    pageString = urllib.request.urlopen(url).read().decode('utf-8')
-   except UnicodeDecodeError:
-    pageString = urllib.request.urlopen(url).read().decode('latin-1').encode('utf-8').decode('utf-8')
-   
-   dom = xml.dom.minidom.parseString(pageString)
-   
-   if not dom.getElementsByTagName('problem_cause'):
-       degree_symbol = chr(176)
-       city = dom.getElementsByTagName('city')[0].getAttribute('data')
-       temp_f = dom.getElementsByTagName('current_conditions')[0].getElementsByTagName('temp_f')[0].getAttribute('data')
-       temp_c = dom.getElementsByTagName('current_conditions')[0].getElementsByTagName('temp_c')[0].getAttribute('data')
-       try:
-        humidity = dom.getElementsByTagName('current_conditions')[0].getElementsByTagName('humidity')[0].getAttribute('data')
-       except:
-        humidity = ""
-       
-       try: 
-        wind = dom.getElementsByTagName('current_conditions')[0].getElementsByTagName('wind_condition')[0].getAttribute('data')
-       except:
-        wind = "" 
-        
-       try:
-        condition = dom.getElementsByTagName('current_conditions')[0].getElementsByTagName('condition')[0].getAttribute('data')
-       except:
-        condition = "" 
-        
-       try:
-        high_f = dom.getElementsByTagName('forecast_conditions')[0].getElementsByTagName('high')[0].getAttribute('data')
-        high_c = str(int((5.0/9.0) * float(int(high_f) -32)))+ degree_symbol + "C"
-        high_f = "High: " + high_f + degree_symbol + "F"
-        low_f = dom.getElementsByTagName('forecast_conditions')[0].getElementsByTagName('low')[0].getAttribute('data')
-        low_c = str(int((5.0/9.0) * float(int(low_f) -32))) + degree_symbol + "C"
-        low_f = "Low: " + low_f + degree_symbol + "F"
-       except:
-           high_f = ""
-           low_f = ""
-           high_c = ""
-           low_c = ""
+    location = e.input
+    if location == "" and user:
+        location = user.get_location(e.nick)
+    location = urllib.parse.quote(location)
+    url = "http://free.worldweatheronline.com/feed/weather.ashx?q={}&format=json&num_of_days=1&key={}".format(location, self.botconfig["APIkeys"]["wwoAPIkey"])
 
-       
-       chanmsg = "%s / %s / %s%sF %s%sC / %s / %s / %s %s - %s %s" % (city, condition, temp_f,degree_symbol, temp_c, degree_symbol, humidity, wind, high_f, high_c,low_f,low_c)
-       e.output = chanmsg
-       return e
-   else:
-       return get_weather2(self, e)
-       
+    response = urllib.request.urlopen(url).read().decode('utf-8')
+    weather = json.loads(response)
+    weather = weather["data"]
+
+    if 'error' not in weather:
+        city = weather['request'][0]['query']
+        desc = weather['current_condition'][0]['weatherDesc'][0]['value']
+        temp = "{}°F {}°C".format(weather['current_condition'][0]['temp_F'], weather['current_condition'][0]['temp_C'])
+        humidity = weather['current_condition'][0]['humidity']
+        wind = "{} at {} mph".format(weather['current_condition'][0]['winddir16Point'], weather['current_condition'][0]['windspeedMiles'])
+        high = "{}°F {}°C".format(weather['weather'][0]['tempMaxF'], weather['weather'][0]['tempMaxC'])
+        low = "{}°F {}°C".format(weather['weather'][0]['tempMinF'], weather['weather'][0]['tempMinC'])
+        outlook = weather['weather'][0]['weatherDesc'][0]['value']
+        message = "{} / {} / {} / Humidity: {} / Wind: {} / High: {} - Low: {} - Outlook: {}".format(city, desc, temp, humidity, wind, high, low, outlook)
+        e.output = message
+        return e
+    else:
+        return get_weather2(self, e)
 
 
 get_weather.command = "!w"
 get_weather.helptext = "Usage: !w <location>\nExample: !w hell, mi\nShows weather info from google.com.\nUse !setlocation <location> to save your location"
-  
+
 def get_weather2(self, e):
     #wunderground weather of place specified in 'zip'
     zip = e.input
     if zip == "" and user:
         zip = user.get_location(e.nick)
-       
+
     url = "http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=" + urllib.parse.quote(zip)
     dom = xml.dom.minidom.parse(urllib.request.urlopen(url))
     city = dom.getElementsByTagName('display_location')[0].getElementsByTagName('full')[0].childNodes[0].data
@@ -87,7 +63,7 @@ def get_weather2(self, e):
             wind = "Wind: " + str(dom.getElementsByTagName('wind_string')[0].childNodes[0].data)
         except:
             humidity = ""
-        
+
         degree_symbol = chr(176)
         chanmsg = "%s / %s / %s%sF %s%sC / %s / %s" % (city, condition, temp_f,degree_symbol, temp_c, degree_symbol, humidity, wind)
         e.output = chanmsg

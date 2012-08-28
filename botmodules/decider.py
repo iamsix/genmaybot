1ff7aa82b72caa88e7991baffcb181e1ade0eee3
cactauz <deansauce@gmail.com>
2012-08-27 23:43:41 -0600

    Update botmodules/decider.py

--- botmodules/decider.py
+++ botmodules/decider.py
@@ -4,8 +4,8 @@ def decider(self, e):
     result = re.search(regex, e.input)
     if result:
         if (random.randint(0,1) == 0):
-            e.output = e.nick + ": " + result.group(0)
-        else:
             e.output = e.nick + ": " + result.group(1)
+        else:
+            e.output = e.nick + ": " + result.group(2)
     return e
 decider.lineparser = True

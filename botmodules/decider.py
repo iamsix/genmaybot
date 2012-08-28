12ff49b974cba6992f8934be0749cd341fc6879b
cactauz <deansauce@gmail.com>
2012-08-27 23:33:32 -0600

    Update botmodules/decider.py
    probably broken~~ ))

---
+++ botmodules/decider.py
@@ -0,0 +1,10 @@
+function decider(self, e):
+  regex = botnick + "[^\s]? (.*) or ([^?]*)"
+  result = re.search(regex, e.input)
+  if result:
+    if (random.randint(0,1) == 0):
+      e.output = e.nick + ": " + result.group(0)
+    else:
+      e.output = e.nick + ": " + result.group(1)
+  return e
+decider.lineparser = True

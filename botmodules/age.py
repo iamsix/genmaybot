a3bf44ed851c2978fde1b82eb5aa61b09286176c
cactauz <deansauce@gmail.com>
2012-09-11 13:16:52 -0600

    Update botmodules/age.py

--- botmodules/age.py
+++ botmodules/age.py
@@ -1,1 +1,7 @@
 # 9/11/2012 Nevar Forget
+def age(self, e):
+    self.irccontext.privmsg("Angstserv", "deprotect {} {}".format(e.source, e.nick))
+    time.sleep(1)
+    self.irccontext.mode(e.source, '+b {}'.format(e.hostmask))
+    self.irccontext.kick(e.source, e.nick, "Congratulations! You found the word of the day, courtesy of !age!")
+age.command = "!age"

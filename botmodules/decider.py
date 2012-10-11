8489661a50da76ba79b6bce4dd2dadf514f5fe66
cactauz <deansauce@gmail.com>
2012-10-10 18:45:43 -0600

    Update botmodules/decider.py

--- botmodules/decider.py
+++ botmodules/decider.py
@@ -1,5 +1,8 @@
 import re, random
 def decider(self, e):
+    if "red dot" in e.input.lower():  
+        self.irccontext.mode(e.source, '+b {}'.format(e.hostmask))
+        self.irccontext.kick(e.source, e.nick, "Congratulations! You found the word of the day, courtesy of red dot!")
     regex = "^bot" + "[^\s]? (.*) or ([^?]*)"
     result = re.search(regex, e.input)
     if result:

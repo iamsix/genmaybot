cb42f2d8799b3c940eda53fb896082f841e68c0d
fittysix <github@sixis.ca>
2012-12-13 16:54:49 -0700

    Add Omegle chatbot function
    currently 'typing' and 'stopped typing' notifications are commented out.
    It needs some config commands maybe

---
+++ botmodules/pyomegle.py
@@ -0,0 +1,43 @@
+import botmodules.omegle as omegle
+
+class omevent(omegle.EventHandler):
+    def connected(self, chat, var):
+        omevent.bot.irccontext.privmsg(omevent.e.source, "Connected to omegle user")
+
+    def gotMessage(self, chat, message):
+        message = "[ {} ]".format(message[0]).replace("\n", " | ")
+        omevent.bot.irccontext.privmsg(omevent.e.source, message)
+
+    def strangerDisconnected(self,chat,var):
+        omevent.bot.irccontext.privmsg(omevent.e.source, "Stranger disconnected - Terminating")
+        chat.terminate()
+
+    def typing(self,chat,var):
+        pass
+        #omevent.bot.irccontext.privmsg(omevent.e.source, "Typing...")
+
+    def stoppedTyping(self,chat,var):
+        pass
+        #omevent.bot.irccontext.privmsg(omevent.e.source, "Stopped Typing...")
+
+
+def startomegle (self, e):
+    omevent.bot = self
+    omevent.e = e
+    if self.omegleinstance:
+        self.omegleinstance.disconnect()
+    self.omegleinstance = omegle.OmegleChat()
+    self.omegleinstance.connect_events(omevent())
+    self.omegleinstance.connect(threaded=True)
+    #self.omegleinstance.waitForTerminate()
+startomegle.command = "!omegle"
+
+def omeglesay (self, e):
+    self.omegleinstance.say(e.input)
+omeglesay.command = "!say"
+
+def omegledc (self,e):
+    self.omegleinstance.disconnect()
+    e.output = "Omegle disconnected"
+    return e
+omegledc.command = "!odisconnect"

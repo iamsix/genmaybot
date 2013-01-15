1e4e1ba6428915c4d81d7d25b521af50f0780331
fittysix <github@sixis.ca>
2013-01-15 04:30:47 -0700

    Add lastfm compare function

--- botmodules/lastfm.py
+++ botmodules/lastfm.py
@@ -4,12 +4,6 @@ import urllib
 import json
 
 
-def __init__(self):
-    self.botconfig["APIkeys"]["lastfmAPIkey"] = "23caa86333d2cb2055fa82129802780a"
-    with open('genmaybot.cfg', 'w') as configfile:
-        self.botconfig.write(configfile)
-
-
 def setlastfmuser(self, e):
     conn = sqlite3.connect('lastfm.sqlite')
     c = conn.cursor()
@@ -26,9 +20,10 @@ setlastfmuser.command = "!setlastfm"
 def nowplaying(self, e):
     conn = sqlite3.connect('lastfm.sqlite')
     c = conn.cursor()
-    lastfmuser = c.execute("SELECT lastfmuser FROM lastfm WHERE user = LOWER(?)", [e.nick]).fetchone()[0]
+    lastfmuser = c.execute("SELECT lastfmuser FROM lastfm WHERE user = LOWER(?)", [e.nick]).fetchone()
 
     if lastfmuser:
+        lastfmuser = lastfmuser[0]
         url = "http://ws.audioscrobbler.com/2.0/?api_key=%s&limit=1&format=json&method=user.getRecentTracks&user=%s" % (self.botconfig["APIkeys"]["lastfmAPIkey"], lastfmuser)
         response = urllib.request.urlopen(url).read().decode('utf-8')
         track = json.loads(response)
@@ -40,3 +35,54 @@ def nowplaying(self, e):
 
     return e
 nowplaying.command = "~np"
+
+def compare(self, e):
+    conn = sqlite3.connect('lastfm.sqlite')
+    c = conn.cursor()
+    user1 = ""
+    user2 = ""
+
+    if len(e.input.split(" ")) == 1:
+        lastfmuser = c.execute("SELECT lastfmuser FROM lastfm WHERE user = LOWER(?)", [e.nick]).fetchone()
+        if lastfmuser:
+            user1 = lastfmuser[0]
+            #first check if we're copmaring against an irc user - if there's a hit it's assumed so
+            compuser = c.execute("SELECT lastfmuser FROM lastfm WHERE user = LOWER(?)", [e.input]).fetchone()
+            if compuser:
+                user2 = compuser[0]
+            else:
+                user2 = e.input
+        else:
+            e.output = "you don't have a last.fm user set up"
+    elif len(e.input.split(" ")) == 2:
+        var1 = e.input.split(" ")[0]
+        var2 = e.input.split(" ")[1]
+        lastfmuser = c.execute("SELECT lastfmuser FROM lastfm WHERE user = LOWER(?)", [var1]).fetchone()
+        if lastfmuser:
+            user1 = lastfmuser[0]
+        else:
+            user1 = var1
+        lastfmuser = c.execute("SELECT lastfmuser FROM lastfm WHERE user = LOWER(?)", [var2]).fetchone()
+        if lastfmuser:
+            user2 = lastfmuser[0]
+        else:
+            user2 = var2
+
+    if user1 and user2:
+        url="http://ws.audioscrobbler.com/2.0/?api_key=%s&method=tasteometer.compare&type1=user&type2=user&value1=%s&value2=%s&format=json" % (self.botconfig["APIkeys"]["lastfmAPIkey"], user1, user2)
+        response = urllib.request.urlopen(url).read().decode('utf-8')
+        match = json.loads(response)
+        score = match['comparison']['result']['score']
+        print(score)
+        artistmatches = match['comparison']['result']['artists']['@attr']['matches']
+        jsonartists = match['comparison']['result']['artists']['artist']
+        artists = []
+        for artist in jsonartists:
+            artists.append(artist['name'])
+        print(artists)
+        artists = ", ".join(artists)
+
+        e.output = "Matching %s and %s :: Score: %s - Artists (%s matches): %s" % (user1, user2, score, artistmatches, artists)
+
+    return e
+compare.command = "~compare"

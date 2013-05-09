645695510cd8b6cf9cc2c69a59253562cc703934
KpaBap <kpabap@gmail.com>
2013-05-08 19:17:03 -0700

    Minor NHL bugs

--- botmodules/nhl.py
+++ botmodules/nhl.py
@@ -7,20 +7,21 @@ def get_nhl_live_games(self, e):
     
     gamestext = "" 
     
-    for game in games:
-        try:
-            progress = game.getElementsByTagName('progress-time')[0].childNodes[0].data
-            awayteam = game.getElementsByTagName('away-team')[0].getElementsByTagName('name')[0].childNodes[0].data
-            hometeam = game.getElementsByTagName('home-team')[0].getElementsByTagName('name')[0].childNodes[0].data
-                    
-            scoreaway = game.getElementsByTagName('away-team')[0].getElementsByTagName('goals')[0].childNodes[0].data
-            scorehome = game.getElementsByTagName('away-team')[0].getElementsByTagName('goals')[0].childNodes[0].data
-            gametext = "%s - %s %s:%s (%s)" % (awayteam, hometeam, scoreaway, scorehome, progress)
-       
+for game in games:
+    gametext = ""
+    try:
+        progress = game.getElementsByTagName('progress-time')[0].childNodes[0].data
+        awayteam = game.getElementsByTagName('away-team')[0].getElementsByTagName('name')[0].childNodes[0].data
+        hometeam = game.getElementsByTagName('home-team')[0].getElementsByTagName('name')[0].childNodes[0].data
+                
+        scoreaway = game.getElementsByTagName('away-team')[0].getElementsByTagName('goals')[0].childNodes[0].data
+        scorehome = game.getElementsByTagName('away-team')[0].getElementsByTagName('goals')[0].childNodes[0].data
+        gametext = "%s - %s %s:%s (%s)" % (awayteam, hometeam, scoreaway, scorehome, progress)
+        if gametext != "":
             gamestext += gametext + " | " 
-       except:
+    except:
         pass
-        
+
 
     gamestext = gamestext[0:-3]
     e.output = gamestext

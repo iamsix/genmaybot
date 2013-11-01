e948e53f301a70844247d5a992b4f22ce8228a0a
KpaBap <kpabap@gmail.com>
2013-11-01 11:06:19 -0700

    Disabling WOTD. Farewell.

--- botmodules/wotd.py
+++ botmodules/wotd.py
@@ -18,7 +18,7 @@ def wotd_trigger(self, e):
         do_wotd(self, e)
     else:
         return
-wotd_trigger.lineparser = True
+wotd_trigger.lineparser = False
 wotd_trigger.wotd = "cat"
 wotd_trigger.wotd_found_timestamp = 0
 wotd_trigger.wotd_setter = ""

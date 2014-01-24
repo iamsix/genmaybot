146fb25781b573a5b6369307399bd3e4bb448738
fittysix <github@sixis.ca>
2014-01-24 01:25:49 -0700

    Fix for random gzipped data
    of course there's no reason for it to be gzipped, since we didn't request it in the first place

--- botmodules/rt.py
+++ botmodules/rt.py
@@ -1,19 +1,35 @@
-import json, urllib.request, urllib.error, urllib.parse, re
+import json
+import urllib.request
+import re
+import gzip
 
 
 def get_rt(self, e):
     url = self.tools['google_url']("site:imdb.com inurl:com/title " + e.input, "imdb.com/title/tt\\d{7}/")
     imdbid = re.search("tt\\d{7}", url).group(0)[2:]
 
-    url = "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?id=%s&type=imdb&apikey=%s" % (imdbid, self.botconfig["APIkeys"]["rtAPIkey"])
-    response = urllib.request.urlopen(url).read().decode('utf-8')
+    url = "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?id={}&type=imdb&apikey={}"
+    url = url.format(imdbid, self.botconfig["APIkeys"]["rtAPIkey"])
+
+    response = urllib.request.urlopen(url).read()
+    try:
+        #RT likes to randomly give us gzipped data even though we never requested it
+        response = gzip.decompress(response)
+    except:
+        pass
+    response = response.decode("utf-8")
     movie = json.loads(response)
     #movie = movie['movies'][0]
     concensus = ""
     if 'critics_consensus' in movie:
         concensus = "- " + movie['critics_consensus']
     url = self.tools['shorten_url'](movie['links']['alternate'])
-    e.output = "%s (%s) - Critics: %s - Users: %s %s [ %s ]" % (movie['title'], str(movie['year']), str(movie['ratings']['critics_score']), str(movie['ratings']['audience_score']), concensus, url)
+    e.output = "%s (%s) - Critics: %s - Users: %s %s [ %s ]" % (movie['title'],
+                                                                str(movie['year']),
+                                                                str(movie['ratings']['critics_score']),
+                                                                str(movie['ratings']['audience_score']),
+                                                                concensus,
+                                                                url)
     return e
 
 get_rt.command = "!rt"

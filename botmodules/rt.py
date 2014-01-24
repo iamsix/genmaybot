979e7bbb8d7e72c552eb5999dec3f93f8e5a4773
fittysix <github@sixis.ca>
2014-01-24 02:02:44 -0700

    Add fallback to RT search if imdb search fails
    there's still a few movies that don't have the associated imdb id.

--- botmodules/rt.py
+++ botmodules/rt.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+import urllib.parse
 import re
 import gzip
 
@@ -11,15 +12,16 @@ def get_rt(self, e):
     url = "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?id={}&type=imdb&apikey={}"
     url = url.format(imdbid, self.botconfig["APIkeys"]["rtAPIkey"])
 
-    response = urllib.request.urlopen(url).read()
-    try:
-        #RT likes to randomly give us gzipped data even though we never requested it
-        response = gzip.decompress(response)
-    except:
-        pass
-    response = response.decode("utf-8")
-    movie = json.loads(response)
-    #movie = movie['movies'][0]
+    movie = loadjson(url)
+    if 'error' in movie:
+        # our IMDB serach didn't work, so lets try rt's normally-useless search
+        url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?q={}&page_limit=1&page=1&apikey={}"
+        url = url.format(urllib.parse.quote(e.input), self.botconfig["APIkeys"]["rtAPIkey"])
+        results = loadjson(url)
+        url = "http://api.rottentomatoes.com/api/public/v1.0/movies/{}.json?apikey={}"
+        url = url.format(results['movies'][0]['id'], self.botconfig["APIkeys"]["rtAPIkey"])
+        movie = loadjson(url)
+
     concensus = ""
     if 'critics_consensus' in movie:
         concensus = "- " + movie['critics_consensus']
@@ -31,5 +33,15 @@ def get_rt(self, e):
                                                                 concensus,
                                                                 url)
     return e
-
 get_rt.command = "!rt"
+
+
+def loadjson(url):
+    response = urllib.request.urlopen(url).read()
+    try:
+        #RT likes to randomly give us gzipped data even though we never requested it
+        response = gzip.decompress(response)
+    except:
+        pass
+    response = response.decode("utf-8")
+    return json.loads(response)

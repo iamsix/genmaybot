ce3d6e8b09af39c1c319ea33c5a948eab924da76
fittysix <github@sixis.ca>
2014-02-24 02:12:12 -0700

    add tvtropes
    Initial checkin, there's probably some bugs here.

---
+++ botmodules/tvtropes.py
@@ -0,0 +1,27 @@
+
+
+def get_trope(self, e, urlposted=False):
+
+    if urlposted:
+        url = e.input
+    else:
+        searchterm = "site:tvtropes.org " + e.input
+        url = self.tools['google_url'](searchterm, "tvtropes.org/pmwiki/pmwiki.php")
+    print(url)
+    trope = ""
+
+    page = self.tools["load_html_from_URL"](url)
+    page = page.select('#wikitext')[0].extract()
+    for div in page.findAll('div'):
+        div.extract()
+
+    print(page)
+    trope = self.tools['remove_html_tags'](str(page))
+    trope = trope[0:510]
+    if trope.rfind(".") != -1:
+        trope = trope[0:trope.rfind(".") + 1]
+
+    e.output = trope
+    return e
+
+get_trope.command = "!trope"

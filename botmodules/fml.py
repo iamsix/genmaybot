b6efd34b4b5e4b5c51ba27e6bea5bff9186ca180
KpaBap <kpabap@gmail.com>
2014-04-27 20:04:31 -0700

    lol whitespace

--- botmodules/fml.py
+++ botmodules/fml.py
@@ -2,19 +2,19 @@ import urllib.request, urllib.error, urllib.parse
 
 def get_fml(self, e):
     #queries a random fmylife.com passage
-	fmlxml = urllib.request.urlopen("http://api.betacie.com/view/random?key=%s&language=en" % self.botconfig["APIkeys"]["fmlAPIkey"]).read().decode('utf-8')
-	start = fmlxml.find("<text>") + 6
-	end = fmlxml.find("</text>")
+    fmlxml = urllib.request.urlopen("http://api.betacie.com/view/random?key=%s&language=en" % self.botconfig["APIkeys"]["fmlAPIkey"]).read().decode('utf-8')
+    start = fmlxml.find("<text>") + 6
+    end = fmlxml.find("</text>")
 
-	fml = fmlxml[start:end]
+    fml = fmlxml[start:end]
 
-	start = fmlxml.find("<agree>") + 7
-	end = fmlxml.find("</agree>")
-    
+    start = fmlxml.find("<agree>") + 7
+    end = fmlxml.find("</agree>")
+
     iAgree = int(fmlxml[start:end])
 
-	start = fmlxml.find("<deserved>") + 10
-	end = fmlxml.find("</deserved>")
+    start = fmlxml.find("<deserved>") + 10
+    end = fmlxml.find("</deserved>")
 
     iDeserved = int(fmlxml[start:end])
 
@@ -23,17 +23,17 @@ def get_fml(self, e):
     iDeserved = round(iDeserved/(iAgree+iDeserved)*100,1)
 
     sAgree = " [FYL: " + str(iAgree))
-	sDeserved = " Deserved it: " + str(iDeserved) + "]"
+    sDeserved = " Deserved it: " + str(iDeserved) + "]"
 
     # Put together the whole line for output
     fml = fml + sAgree + sDserved
-    
-    fml = fml.replace('&quot;', '"')
-	fml = fml.replace('&amp;quot;', '"')
-	fml = fml.replace('&amp;', "&")
-	e.output = self.tools['decode_htmlentities'](fml)
 
-	return e
+    fml = fml.replace('&quot;', '"')
+    fml = fml.replace('&amp;quot;', '"')
+    fml = fml.replace('&amp;', "&")
+    e.output = self.tools['decode_htmlentities'](fml)
+
+    return e
 
 get_fml.command = "!fml"
 get_fml.helptext = "Usage: !fml\nShows a random entry from fmylife.com"

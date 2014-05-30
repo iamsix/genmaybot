e4919cc06f32b3b7c707186a59c5dab9ab9a0df3
cactauz <deansauce@gmail.com>
2014-05-29 21:06:19 -0600

    Update aviationWeather.py

--- botmodules/aviationWeather.py
+++ botmodules/aviationWeather.py
@@ -7,8 +7,10 @@ def metar(self, e):
   + 'dataSource=metars&requestType=retrieve&format=xml&stationString=' \
   + station \
   + '&hoursBeforeNow=2&mostRecent=true'
-  xml = ET.parse(urllib.request.urlopen(url))
+  response = urllib.request.urlopen(url)
+  xml = ET.parse(response)
   print(url)
+  print(response)
   print(xml)
   e.output = xml.getroot().find('raw_text').text
   return e

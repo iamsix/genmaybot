ae551eeb14df66083e0483a6e11f0117704f120b
cactauz <cactauz@users.noreply.github.com>
2015-01-07 13:19:39 -0700

    Update kick.py

--- botmodules/kick.py
+++ botmodules/kick.py
@@ -1,3 +1,3 @@
 def do_kick(self, e):
-    self.irccontext.kick("#auzland", e.nick, "thank you for participating in this test (by %s)" % e.nick)
+    self.irccontext.kick("#auzland", e.nick, "thank you for participating in this test (by mixomatosys)")
 do_kick.command = "!kick"

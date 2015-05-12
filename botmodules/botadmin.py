3718509558829a7a933237a91a623e24b4cc96a6
KpaBap <kpabap@gmail.com>
2015-05-11 20:41:39 -0700

    Update botadmin.py

--- botmodules/botadmin.py
+++ botmodules/botadmin.py
@@ -2,7 +2,7 @@ import time, sys, traceback
 import threading
 
 def monitor_pm(line,nick,self,c):
-	return ", ".join((line, nick))
+
 	if line.split(" ")[1] == "on":
 	    return "Enabled PM monitoring. All PMs will be mirrored to you."	
 	elif line.split(" ")[1] == "off": 

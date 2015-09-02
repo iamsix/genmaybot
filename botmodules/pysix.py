71397e1993e1319282557735e60e1ea0214ed2fe
fittysix <github@sixis.ca>
2015-09-02 03:26:05 -0600

    new cleverbot api
    fix this file to use it

--- botmodules/pysix.py
+++ botmodules/pysix.py
@@ -1,6 +1,5 @@
-from botmodules.chatterbotapi import ChatterBotFactory, ChatterBotType
 import re
-
+import cleverbot
 
 def chatter(self, e):
     botnick = e.botnick.lower()
@@ -9,12 +8,9 @@ def chatter(self, e):
         if e.input[0:len(botnick)].lower() == botnick:
             e.input = e.input[len(botnick):].strip()
         e.input = re.sub("(?i)%s" % botnick, "cleverbot", e.input)
-        factory = ChatterBotFactory()
+        cleverbot_client = cleverbot.Cleverbot()
+        e.output = cleverbot_client.ask(e.input)
 
-        bot1 = factory.create(ChatterBotType.CLEVERBOT)
-        bot1session = bot1.create_session()
-
-        e.output = bot1session.think(e.input)
         e.output = self.tools['decode_htmlentities'](re.sub("(?i)cleverbot", botnick, e.output))
         return e
     else:
